33e5f385b45dcae930d0e4c5d724983009fccfee
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2020-08-10 18:27:06 -0700

    1,2

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,24 +2,44 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+int score = 0;
 		
-	}
+String input = JOptionPane.showInputDialog("what is more dangerous to steel than stone"); 
+		if (input.equals("lava")) {
+		score += 1;	 
+		JOptionPane.showMessageDialog(null, "correct!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);
+		}
+	else{
+		JOptionPane.showMessageDialog(null, "wrong, the awnser is lava!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);	
+			}
+		String input2 = JOptionPane.showInputDialog("what is are the animals that create sweet goo that bears love"); 
+		if (input2.equals("honeybees")) {
+		score += 1;	 
+		JOptionPane.showMessageDialog(null, "correct!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);
+		}
+	else{
+		JOptionPane.showMessageDialog(null, "wrong, the awnser is honeybees!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);	
+			}		
+		String input3 = JOptionPane.showInputDialog("what is the bird that that swims noy flies"); 
+		if (input3.equals("peinguins")) {
+		score += 1;	 
+		JOptionPane.showMessageDialog(null, "correct!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);
+		}
+	else{
+		JOptionPane.showMessageDialog(null, "wrong, the awnser is peinguins!");
+		JOptionPane.showMessageDialog(null, "your score is " + score);	
+			}	 
 }
-
+}
