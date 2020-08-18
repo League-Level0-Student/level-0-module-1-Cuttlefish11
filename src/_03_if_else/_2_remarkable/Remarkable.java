107b70dc9b06af3b15597d7460b2c6443c54d313
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2020-08-17 18:26:56 -0700

    1,3

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -10,9 +10,26 @@ public static void main(String[] args) {
 		// 1. Save something remarkable about each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String input = JOptionPane.showInputDialog(" Write a name of someone remarkable?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+	if (input.equals("Bob")) {
+		  JOptionPane.showMessageDialog(null, input + " is remarkable for reasons");
+	}	
+	else if (input.equals("Shorty")) {
+		  JOptionPane.showMessageDialog(null, input + " is remarkable for their tallness");
+	}	
+	else if (input.equals("Albert")) {
+		  JOptionPane.showMessageDialog(null, input + " is remarkable for obvious reason");
 	}
+
+	  else {
+			JOptionPane.showMessageDialog(null, input + " isn't remarkable.");
+			
+		}
+
+
+
+
+}
 }
 
--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,22 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("what is the passcode?");
 	
 	// 1. Set a password in a String variable
-	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
-
-	// 4. If their guess matches the password, show them the secret message
+		if (input.equals("passcode")) {
+			  JOptionPane.showMessageDialog(null, "This is very important seceret message...");
+		}			  
+			  else {
+					JOptionPane.showMessageDialog(null, "INTRUDER!!");
+					
+				}
 
 	// 5. If the password does not match, pop-up "INTRUDER!!"
+		
+}}
 
-}
+	
+		
+	
